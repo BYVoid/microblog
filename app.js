ad4bd3fc100afa3b8d3138181859baad56cd45db
iT2afL0rd <it2afl0rd@gmail.com>
2014-11-04 11:10:35 +0800

    update to version 0.0.2, change to use express 4.x

--- app.js
+++ app.js
@@ -1,59 +1,105 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+var partials = require('express-partials');
+var mysession = require('express-session');
+var flash = require('connect-flash');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(flash());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var MongoStore = require('connect-mongo')(mysession);
 var settings = require('./settings');
-var MongoStore = require('connect-mongo');
 
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({
+// store session data in MongoDB
+app.use(mysession({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
     secret: settings.cookieSecret,
     store: new MongoStore({
-      db: settings.db
+        db: settings.db
     })
-  }));
-  app.use(express.router(routes));
-  app.use(express.static(__dirname + '/public'));
+}));
+
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user;
+    next();
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-app.dynamicHelpers({
-  user: function(req, res) {
-    return req.session.user;
-  },
-  error: function(req, res) {
-    var err = req.flash('error');
+app.use(function (req, res, next) {
+    err = req.flash('error');
     if (err.length)
-      return err;
+        res.locals.error = err;
     else
-      return null;
-  },
-  success: function(req, res) {
-    var succ = req.flash('success');
-    if (succ.length)
-      return succ;
-    else
-      return null;
-  },
+        res.locals.error = null;
+    next();
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.use(function (req, res, next) {
+    succ = req.flash('success');
+    if (succ.length)
+        res.locals.success = succ;
+    else
+        res.locals.success = null;
+    next();
+});
+
+app.use(partials());
+
+app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+
+
+module.exports = app;
